1d6ac3883df395fb37172b13668a4b69be63f432
FangAnger <74387929+FangAnger@users.noreply.github.com>
2020-11-23 16:50:35 +0800

    v1.2
    init

--- src/main/java/com/ibm/springboot/SpringbootApplication.java
+++ src/main/java/com/ibm/springboot/SpringbootApplication.java
@@ -1,16 +1,18 @@
 package com.ibm.springboot;
 
 import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
+import org.springframework.context.annotation.Configuration;
+
 
 //@EnableAsync //开启异步注解功能
 //@EnableScheduling //开启基于注解的定时任务
 @MapperScan(value = "com.ibm.springboot.mapper")
 @SpringBootApplication
+@Configuration
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
